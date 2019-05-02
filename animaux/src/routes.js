628b9dd4f8f500410afe4f7f5ec96c3341fec099
szczepan27 <s.szymon1127@gmail.com>
2019-05-02 20:49:34 +0200

    adding routes

--- animaux/src/routes.js
+++ animaux/src/routes.js
@@ -1,9 +1,13 @@
-// import  from  './components/'
-// import  from  './components/'
+import Home from  './components/Home.vue'
+import aboutUs from  './components/aboutUs.vue'
+import Kontakt from './components/Kontakt.vue'
+import Doggos from './components/Doggos.vue'
+import Cattos from './components/Cattos.vue'
 
-export default{
-    // { path: '', component: },
-    // { path: '', component: },
-    // { path: '', component: },
-    // { path: '', component: }
-}
+export default[
+    { path: '/', component: Home },
+    { path: '/about', component: aboutUs },
+    { path: '/contact', component: Kontakt},
+    { path: '/doggos', component: Doggos},
+    { path: '/cattos', component: Cattos}
+]
