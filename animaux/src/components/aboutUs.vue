6d5b52613d19c1b3811cdee1b30b3472edb8b12b
hunchoradek <49205334+hunchoradek@users.noreply.github.com>
2019-05-03 00:54:07 +0200

    AboutUs
    Added About Us page and made it responsible

--- animaux/src/components/aboutUs.vue
+++ animaux/src/components/aboutUs.vue
@@ -1,15 +1,49 @@
 <template>
     <div id="app-about-us">
-        <p>Tu będzie coś o nas</p>
+        <h1 class="display-4">Nasz Zespół: </h1>
+        <p class="display-2 grey--text">3 debili ze szkolki sie dobralo i robia jakies gowno dojrbane o pjeskach i kotach</p>
+
+        <v-container class="my-3">
+
+            <v-layout row wrap>
+                <v-flex xs12 sm6 md4 lg4 v-for="person in team" :key="person.name">
+                    <v-card class="text-xs-center ma-3">
+                        <v-responsive class="pt-4">
+                            <v-avatar size="300" class="grey lighten-1">
+                                <img :src="person.avatar">
+                            </v-avatar>
+                        </v-responsive>
+                        <v-card-text>
+                            <div class="display-2">{{ person.name }}</div>
+                            <div class="grey--text headline">{{ person.role }}</div>
+                        </v-card-text>
+                    </v-card>
+                </v-flex>
+            </v-layout>    
+        </v-container>
     </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return{
+            team: [
+                { name: 'Radek Olesiński', role: 'Projektant strony i pomysłodawca', avatar: '/avatar-1.png'},
+                { name: 'Szymon Szczepanowski', role: 'Projektant strony i pomysłodawca', avatar: '/avatar-2.png'},
+                { name: 'Aleksandra Kacprzak', role: 'Projektantka strony i pomysłodawca', avatar: '/avatar-3.png'},
+            ]
+        }
+    }
 }
 </script>
 
 <style scoped>
-
+.display-4 {
+text-align: center;
+}
+p {
+    text-align: center;
+    padding: 1em;
+}
 </style>
